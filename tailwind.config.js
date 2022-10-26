866a63ca0bd9ed92c4d15fb0876ab8b019519d6a
Ibrohimbek <ismailovibrokhim98@gmail.com>
2022-10-26 16:43:33 +0500

    Add examle change

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,61 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        black:{
+          black_dark:"#24283A",
+          black_thin:"#464A4D"
+        },
         blue: {
-          bright: '#109EF4',
+          accent: '#109EF4',
+          base: '#2B3D90',
+          recommend: '#22B0F8',
+        },
+        gray: {
+          bg_nav: '#FAFAFA',
+          bg_main: '#F7F9FA',
+          text: '#7A7E80',
+          placeholder:"#B4B6B8"
+        },
+        red: {
+          xit: '#FF3A3A',
+          sale: '#FF5C5C',
+        },
+        green: {
+          new: '#0BCC23',
+        },
+        yellow:{
+          popular:"#FAB529"
         }
       },
+      width: {
+        container: '1220px',
+        cardWidth: '280px',
+        resCardWidth:"220px",
+        orderBtn:"248px",
+        resOrderBtn:"196px",
+        '410': '410px',
+        '426': '426px',
+        '219': '219px'
+      },
+      height: {
+        cardHeight: '220px',
+        resCardHeight:'347px',
+        orderBtn:"48px",
+        '500': '500px',
+        '420': '420px',
+        '430': '430px',
+        '520': '520px',
+        '577': '577px',
+      },
+      boxShadow: {
+        order_btn:
+          '0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)',
+      },
+      borderRadius: {
+        labelRadius:'0px 8px 8px 0px',
+        btn_radius:"12px"
+      },
+
     },
   },
   plugins: [],
