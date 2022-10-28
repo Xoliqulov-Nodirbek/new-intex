9f08ca8a14e849cc3b906866c64c5f9c6d3d7383
Nurillo18 <suyunboyevnurillo18@gmail.com>
2022-10-28 14:38:32 +0500

    Add all sections and fix header

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,10 +1,20 @@
+import AboutUs from '../AboutUs/AboutUs'
 import Hero from '../Hero/Hero'
+import NewProduct from '../NewProduct/NewProduct'
+import Popular from '../Popular/Popular'
+import SellCommon from '../SellCommon/SellCommon'
+import TashkentPools from '../TashkentPools/TashkentPools'
 
 function Main() {
   return (
     <main>
         <Hero/>{/*Nurillo Hero oldi*/}
-        {/* <Popular/> Nozimjon Популярные товары oldi */}
+        <Popular/>  {/*Nozimjon Популярные товары oldi */}
+        <TashkentPools/> {/*Nodirbek tashkent sectionni oldi*/}
+        <NewProduct/> {/*Nozimjon New Product sectionni oldi*/}
+        <SellCommon/> {/*Ibrohim sell common sectionni oldi*/}
+        <AboutUs/> {/* Nurillo About sectionni oldi */}
+
     </main>
   )
 }
---
+++ components/NewProduct/NewProduct.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewProduct() {
+  return (
+    <div>
+        {/* Nozimjon Yozmoqda */}
+    </div>
+  )
+}
+
+export default NewProduct
