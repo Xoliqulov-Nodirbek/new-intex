e9b248dcec4f017185ea1e6c08f5b47d2e7e8bbb
Nurillo18 <suyunboyevnurillo18@gmail.com>
2022-10-31 11:09:17 +0500

    Fix all section errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,25 +11,16 @@ module.exports = {
         5.5: "5.5px",
         13: "13px",
       },
-      colors: {
-        black: {
-          black_dark: "#24283A",
-          black_thin: "#464A4D",
-          black_light: "#A0A0A0"
-
       screens: {
-
         tablet: '850px',
-
-        'tablet': '850px',
-        'mini_tablet': '790px',
+        mini_tablet: '790px',
 
       },
       colors: {
         black: {
           black_dark: '#24283A',
           black_thin: '#464A4D',
-
+          black_light: "#A0A0A0"
         },
 
         blue: {
@@ -67,35 +58,21 @@ module.exports = {
           popular: "#FAB529",
         },
       },
-
       backgroundColor: {
         azure: "#F2F2F2",
         blueActive: "#109EF4",
       },
-
-      fontSize: {
-        32: "32px",
-
-          popular: '#FAB529',
-        },
-
-      },
-
       lineHeight: {
         36: "36px",
         22: '22px'
       },
-
       fontFamily: {
         inter: "Inter",
       },
-
       width: {
-
         heroImg: '571px',
+        heroContent:'450px',
         heroImgMobile: '320px',
-
-
         respo: "100%",
         cardWidth: "280px",
         resCardWidth: "220px",
@@ -106,24 +83,13 @@ module.exports = {
         219: "219px",
         111: '111px'
       },
-      maxWidth: {
-        container: "1220px",
-        248: '225px'
-      },
       height: {
         cardHeight: "220px",
         resCardHeight: "347px",
         orderBtn: "48px",
-        500: "500px",
-        420: "420px",
-        430: "430px",
-        520: "520px",
-        577: "577px",
-
         tashkentPoolsImg: '478px',
         heroImg:'571px',
         heroImgMobile:'320px',
-
         container: '1220px',
         heroContent: '447px',
         cardWidth: '280px',
@@ -132,14 +98,32 @@ module.exports = {
         resCardWidth: '220px',
         orderBtn: '248px',
         resOrderBtn: '196px',
+        heroimgHeight: '319px',
+        heriImgMobile: '160px',
         '410': '410px',
         '426': '426px',
         '219': '219px',
+        '500': '500px',
+        '420': '420px',
+        '430': '430px',
+        '520': '520px',
+        '577': '577px',
+        '312': '312px',
+        '237': '237px',
+        '264': '264px',
+        '207': '207px',
+        '153': '153px',
+        '135': '135px',
+        '93': '93px',
+        '83': '83px',
+        '48': '48px',
+        '83': '83px'
       },
       maxWidth: {
         container: '1220px',
         cont: '10.5%',
         '426': '426px',
+        '248': '225px',
         '403': '403px',
         '362': '362px',
         '281': '281px',
@@ -153,42 +137,14 @@ module.exports = {
         '430': '430px',
         '237': '237px',
       },
-      height: {
-        heroimgHeight: '319px',
-        heriImgMobile: '160px',
-        cardHeight: '220px',
-        resCardHeight: '347px',
-        orderBtn: '48px',
-        '500': '500px',
-        '420': '420px',
-        '430': '430px',
-        '520': '520px',
-        '577': '577px',
-        '312': '312px',
-        '237': '237px',
-        '264': '264px',
-        '207': '207px',
-        '153': '153px',
-        '135': '135px',
-        '93': '93px',
-
-        '83': '83px',
-        '48': '48px',
-
-        '83': '83px'
-
-
-      },
       boxShadow: {
         order_btn:
           "0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)",
           card: '2px 12px 24px rgba(60, 60, 60, 0.1)'
       },
       borderRadius: {
-
         labelRadius: "0px 8px 8px 0px",
         btn_radius: "12px",
-
         labelRadius: '0px 8px 8px 0px',
         btn_radius: '12px',
         sectionRadius: "20px",
