aa0ee7d79d06f03d34b61ed1afcf7148dcb5f9c8
kholiqulov_nodirbek <99204049+Xoliqulov-Nodirbek@users.noreply.github.com>
2022-11-01 17:25:05 +0500

    Add new ch

--- components/BuyAll/BuyAll.js
+++ components/BuyAll/BuyAll.js
@@ -9,7 +9,6 @@ const BuyAll = () => {
         <div className="w-full md:h-520 my-10 md:my-20 flex flex-col md:flex-row items-center justify-between px-5 md:px-0 rounded-sectionRadius bg-gray-bg_main py-10 md:pl-10  ">
           <div>
             <h2 className="text-lg sm:text-3xl md:text-32 font-bold leading-9">
-
               Купить оптом
             </h2>
             <p className="max-w-403 text-sm md:text-base text-gray-foot leading-5 mt-4 pb-6 sm:pb-8 ">
@@ -17,34 +16,31 @@ const BuyAll = () => {
               не менее 20 товаров.
             </p>
             <div className=" md:hidden w-full flex items-center justify-center layout">
-                <Image
-                  className="w-335 h-207"
-                  src={'/Assets/Images/KarkasHero1.png'}
-
-                  alt="Picture of the Karkas"
-                  width={335}
-                  height={202}
-                  priority="true"
-                  layout="responsive"
-                />
-            </div>
-              <BuyForm />
-          </div>
-            <div className="hidden md:block ml-4 lg:ml-0">
               <Image
-                className="w-600 max-h-380"
-                src={'/Assets/Images/KarkasHero.png'}
-
+                className="w-335 h-207"
+                src={"/Assets/Images/KarkasHero1.png"}
                 alt="Picture of the Karkas"
-                width={628}
-                height={380}
+                width={335}
+                height={202}
                 priority="true"
+                layout="responsive"
               />
             </div>
-
+            <BuyForm />
+          </div>
+          <div className="hidden md:block ml-4 lg:ml-0">
+            <Image
+              className="w-600 max-h-380"
+              src={"/Assets/Images/KarkasHero.png"}
+              alt="Picture of the Karkas"
+              width={628}
+              height={380}
+              priority="true"
+            />
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
